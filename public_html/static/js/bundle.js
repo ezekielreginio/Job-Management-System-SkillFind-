abcf02cd8d908f41ff17fcfdb47231ee0a5af8aa
ezekielreginio <ezekielreginio025@gmail.com>
2021-04-01 11:36:53 +0800

    #

--- public_html/static/js/bundle.js
+++ public_html/static/js/bundle.js
@@ -285,11 +285,12 @@ if (location.href.indexOf('skills') != -1) {
 
 if (location.href.indexOf('employer/addjob') != -1) {
   let multi_checkbox = document.getElementsByClassName("custom-checkbox");
+  theCheckbox = document.querySelector('input');
 
   for (let i = 0; i < multi_checkbox.length; i++) {
     multi_checkbox[i].classList.add("checkbox-border", "my-3");
-    multi_checkbox[i].addEventListener("click", function () {
-      this.firstElementChild.click();
+    multi_checkbox[i].addEventListener("click", function (e) {
+      if (this.firstElementChild.checked) this.firstElementChild.checked = false;else this.firstElementChild.checked = true;
     });
   }
 }
--- src/employer_dashboard/addjob.js
+++ src/employer_dashboard/addjob.js
@@ -1,10 +1,16 @@
 
 if(location.href.indexOf('employer/addjob') != -1){
     let multi_checkbox = document.getElementsByClassName("custom-checkbox")
+    theCheckbox = document.querySelector('input');
     for(let i = 0; i<multi_checkbox.length; i++){
         multi_checkbox[i].classList.add("checkbox-border", "my-3")
-        multi_checkbox[i].addEventListener("click", function(){
-            this.firstElementChild.click()
+        multi_checkbox[i].addEventListener("click", function(e){
+            if(this.firstElementChild.checked)
+                this.firstElementChild.checked = false
+            else
+                this.firstElementChild.checked = true
         })
     }
+
+    
 }
