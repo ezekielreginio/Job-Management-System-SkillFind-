299c3fc4e9259360c416321e2f98a0e96d7d19c3
ezekielreginio <78013755+ezekielreginio@users.noreply.github.com>
2021-04-18 12:08:53 +0800

    #

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn _skillfind.wsgi
--- src/app_handicapped/index.js
+++
@@ -1,9 +0,0 @@
-
-if(location.href.indexOf('handicapped') != -1 ){
-    document.getElementById("nav-home").remove()
-    document.getElementById("dropdownId").remove()
-    document.getElementById("btn-login-applicant").remove()
-    document.getElementById("btn-handicap-ui").remove()
-
-      
-}
